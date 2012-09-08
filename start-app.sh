792df6d90d859f3e60d338d7727070d2149735a9
Shirish Padalkar <shirish4you@gmail.com>
2012-09-08 19:12:26 +0530

    Modified startup script to start mongodb before application deployment. Some tests needs database call

--- start-app.sh
+++ start-app.sh
@@ -1,3 +1,8 @@
+echo "Starting Mongo DB..."
+mkdir db
+mkdir logs
+mongod --fork --dbpath ./db/ --logpath ./logs/mongod.log
+
 echo "Starting tomcat..."
 ./devtools/apache-tomcat-7.0.28/bin/startup.sh 
 
@@ -5,11 +10,6 @@ echo "Build & Deploy application..."
 cd code
 mvn tomcat7:deploy
 cd ..
-
-echo "Starting Mongo DB..."
-mkdir db
-mkdir logs
-mongod --fork --dbpath ./db/ --logpath ./logs/mongod.log
 
 echo "Opening in browser ..."
 open http://localhost:8080/placement &
